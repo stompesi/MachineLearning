55eeae6f580a366c6db64d74de38945bf5b1a59b
Stompesi <stompesi@gmail.com>
2015-03-05 18:45:43 +0900

    "Decision Tree" 스터디중(2)

---
+++ code/treePlotter.py
@@ -0,0 +1,110 @@
+#-*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+
+#파이썬은 기본적인 모든 변수가 전역 변수
+
+# 상자와 화살표 형태 정의
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+# 화살표로 주석 그리기
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+  createPlot.ax1.annotate(nodeTxt, xy=parentPt,
+                          xycoords='axes fraction',
+                          xytext=centerPt, textcoords='axes fraction',
+                          va="center", ha="center", bbox=nodeType,
+                          arrowprops=arrow_args)
+
+def createPlot():
+  fig = plt.figure(1, facecolor='white')
+  flg.clf()
+  # 플롯
+  createPlot.ax1 = plt.subplot(111, frameon=False)
+  plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
+  plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+  plt.show()
+
+def getNumLeafs(myTree):
+  numLeafs = 0
+  firstStr = myTree.keys()[0]
+  secondDict = myTree[firstStr]
+  for key in secondDict.keys():
+    if type(secondDict[key]).__name__ == 'dict':
+      numLeafs += getNumLeafs(secondDict[key])
+    else:
+      numLeafs += 1
+
+  return numLeafs
+
+def getTreeDepth(myTree):
+  maxDepth = 0
+  firstStr = myTree.keys()[0]
+  secondDict = myTree[firstStr]
+  for key in secondDict.keys():
+    if type(secondDict[key]).__name__ == 'dict':
+      thisDepth = 1 + getTreeDepth(secondDict[key])
+    else:
+      thisDepth = 1
+    if thisDepth > maxDepth:
+      maxDepth = thisDepth
+
+  return maxDepth
+
+def retrieveTree(i):
+  listOfTrees = [{'no surfacing' : {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+                 {'no surfacing' : {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
+                ]
+
+  return listOfTrees[i]
+
+# 자식 노드와 부모 노드 사이에 텍스트 플롯하기
+def plotMidText(cntrPt, parentPt, txtString):
+  xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+  yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+  createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+  numLeafs = getNumLeafs(myTree)
+  getTreeDepth(myTree)
+  firstStr = myTree.keys()[0]
+  cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+  plotMidText(cntrPt, parentPt, nodeTxt)
+  plotNode(firstStr, cntrPt, parentPt, decisionNode)
+  secondDict = myTree[firstStr]
+  plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+  for key in secondDict.keys():
+    if type(secondDict[key]).__name__ == 'dict':
+      plotTree(secondDict[key], cntrPt, str(key))
+    else:
+      plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+      plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+      plotMidText((plotTree.xOffm plotTree.yOff), cntrPt, str(key))
+  plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+def createPlot(inTree):
+  fig = plt.figure(1, facecolor='white')
+  fig.clf()
+  axprops = dict(xticks=[], yticks=[])
+  createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+  plotTree.totalW = float(getNumLeafs(inTree))
+  plotTree.totalD = float(getTreeDepth(inTree))
+  plotTree.xOff = -0.5 / plotTree.totalW
+  plotTree.yOff = 1.0
+  plotTree(inTree, (0.5, 1.0), '')
+  plt.show()
+
+
+
+# 69부터 
+
+
+
+
+
+
+
+
+
+
+
--- code/trees.py
+++ code/trees.py
@@ -1,4 +1,45 @@
+#-*- coding: utf-8 *-
+import operator
 from math import log
+
+def majorityCnt(classList):
+  classCount = {}
+  for vote in classList:
+    if vote not in classCount.keys():
+      classCount[vote] = 0
+    classCount[vote] += 1
+
+  sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+  return sortedClassCount[0][0]
+
+def createTree(dataSet, labels):
+  classList = [example[-1] for example in dataSet]
+
+  # 모든 분류 항목이 같을 때 멈춤
+  if classList.count(classList[0]) == len(classList):
+    return classList[0]
+
+  # 속성이 더 이상 없을 때 가장 많은 수를 반환함
+  if len(dataSet[0]) == 1:
+    return majorityCnt(classList)
+
+  bestFeat = chooseBestFeatureToSplit(dataSet)
+  bestFeatLabel = labels[bestFeat]
+  myTree = {
+    bestFeatLabel: {}
+  }
+  del(labels[bestFeat])
+  # 유일한 값의 리스트를 구함
+  featValues = [example[bestFeat] for example in dataSet]
+  uniqueVals = set(featValues)
+
+  for value in uniqueVals:
+    subLabels = labels[:]
+    myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+
+  return myTree
+
 
 def calcShannonEnt(dataSet):
   numEntries = len(dataSet) # 데이터 집합에 있는 사례 개수
@@ -17,12 +58,14 @@ def calcShannonEnt(dataSet):
   return shannonEnt
 
 def createDataSet():
-  dataSet = [[1, 1, 'yes'],
-             [1, 1, 'yes'],
-             [1, 0, 'no'],
-             [1, 1, 'no'],
-             [1, 1, 'no']]
-  labels = ['no surfacing', 'flippers']
+  dataSet = [
+              [1, 1, 'yes'],
+              [1, 1, 'yes'],
+              [1, 0, 'no'],
+              [1, 1, 'no'],
+              [1, 1, 'no']
+            ]
+  labels  = ['no surfacing', 'flippers']
   return dataSet, labels
 
 
@@ -47,3 +90,33 @@ def splitDataSet(dataSet, axis, value):
       reduceFeatVec.extend(featVec[axis+1:])
       retDataSet.append(reduceFeatVec)
   return retDataSet
+
+# 데이터 집합 분할하기
+def chooseBestFeatureToSplit(dataSet):
+  numFeatures = len(dataSet[0]) - 1
+  baseEntropy = calcShannonEnt(dataSet)
+  bestInfoGain = 0.0
+  bestFeature = -1
+
+  for i in range(numFeatures):
+    # 분류 항목 표시에 대해 중복이 없는 리스트 생성
+    featList = [example[i] for example in dataSet]
+    uniqueVals = set(featList)
+    newEntropy = 0.0
+
+    # 각각의 분할을 위해 엔트로피 계산
+    for value in uniqueVals:
+      subDataSet = splitDataSet(dataSet, i, value)
+      prob = len(subDataSet) / float(len(dataSet))
+      newEntropy += prob * calcShannonEnt(subDataSet)
+
+    infoGain = baseEntropy - newEntropy
+
+    # 가장 큰 정보 이득 찾기
+    if(infoGain > bestInfoGain):
+      bestInfoGain = infoGain
+      bestFeature = i
+
+  return bestFeature
+
+# 재귀적으로 트리 만들기
