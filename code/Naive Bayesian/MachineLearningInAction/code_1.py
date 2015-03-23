84e2b12334c8f5b5542eb421ff65e44e0697a063
Stompesi <stompesi@gmail.com>
2015-03-24 01:13:32 +0900

    no message

---
+++ code/Naive Bayesian/MachineLearningInAction/code_1.py
@@ -0,0 +1,6 @@
+def bag_of_words_2_vec_MN(vocab_list, input_set):
+    return_vec = [0] * len(vocab_list)
+    for word in input_set:
+        if word in vocab_list:
+            return_vec[vocab_list.index(word)] += 1
+    return return_vec
